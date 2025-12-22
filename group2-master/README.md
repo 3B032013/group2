b65d2b38ea7e870ae1b21f87b1aeaa9ac122c065
mikij0504 <mikij0504@gmail.com>
2025-12-23 01:47:39 +0800

    remove

--- group2-master/README.md
+++
@@ -1,7 +0,0 @@
-# 啟動方式
-1. 請建立虛擬環境(venv/conda) 並安裝 pip install -r requirements.txt
-2. 請在 /group2 資料夾當中執行 python app.py
-
-## 系統畫面
-首頁
-<img src="https://duk.tw/fTJ9c1.png" alt="Kaggle score" width="100%">
--- group2-master/app.py
+++
@@ -1,9 +0,0 @@
-# app.py
-from application import create_app
-
-# 呼叫 application/__init__.py 裡面的 create_app 函式
-app = create_app()
-
-if __name__ == '__main__':
-    # 這裡啟動 server，Debug 模式可開啟方便除錯
-    app.run(debug=True, port=8000)
